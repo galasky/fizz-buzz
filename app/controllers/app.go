9ee4f5cbd4edfa35b0185fb22b102af0d7458340
galasky <ttrebmal@gmail.com>
2018-03-14 17:45:12 +0100

    first commit

---
+++ app/controllers/app.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+	"strconv"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func addResult(string1, string2 string, int1, int2, number int) string {
+	if number % int1 == 0 {
+		if number % int2 == 0 {
+			return string1 + string2
+		}
+		return string1
+	}
+	if number % int2 == 0 {
+		return string2
+	}
+	return strconv.Itoa(number)
+}
+
+func (c App) Generate(string1, string2 string, int1, int2, limit int) revel.Result {
+	var results []string
+
+	for i := 1; i < limit; i++ {
+		results = append(results, addResult(string1, string2, int1, int2, i))
+	}
+
+	return c.RenderJSON(results)
+}
